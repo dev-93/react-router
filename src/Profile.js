1c06f0995e0e22141c61bcf527016afb5bede1d9
dev-93 <cr000@naver.com>
2020-10-18 20:01:49 +0900

    match객체 안의 params 값 참조하여 렌더링

---
+++ src/Profile.js
@@ -0,0 +1,33 @@
+import React from "react";
+
+const data = {
+    tt: {
+        name: "김태남",
+        description: "프론트엔드 개발자",
+    },
+    jiji: {
+        name: "서지훈",
+        description: "김태남 친구 중 하나",
+    },
+};
+
+const Profile = ({ match }) => {
+    console.log(match);
+    const { username } = match.params;
+    const profile = data[username];
+
+    if (!profile) {
+        return <p>존재하지 않는 사용자입니다.</p>;
+    }
+
+    return (
+        <div>
+            <h3>
+                {username}({profile.name})
+            </h3>
+            <p>{profile.description}</p>
+        </div>
+    );
+};
+
+export default Profile;
