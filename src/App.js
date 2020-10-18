d9a51b1ba9d098cbe15da67cab4181ae88336426
dev-93 <cr000@naver.com>
2020-10-18 19:43:59 +0900

    react-router 기초, Browser Router, Route로 해당 component 보여주기

--- src/App.js
+++ src/App.js
@@ -1,26 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Route } from "react-router-dom";
+import About from "./About";
+import "./App.css";
+import Home from "./Home";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Route exact path="/" component={Home} />
+            <Route path="/about" component={About} />
+        </div>
+    );
 }
 
 export default App;
---
+++ src/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Home = () => {
+    return (
+        <div>
+            <h1>홈</h1>
+            <p>제일 첫번째 페이지</p>
+        </div>
+    );
+};
+
+export default Home;
